b4c0f72a56d80d8e191bb1650a17590af33eef79
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-07-09 01:16:18 +0200

    Index.html read from file

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-  }); 
+var indexToDisplay = fs.readFileSync('index.html');
+var buf = indexToDisplay.toString();
 
-//var indexToDisplay = fs.readFileSync('index.html');
-//console.log(indexToDisplay);
+app.get('/', function(request, response) {
+	//response.send('Hello World 22!');
+	response.send(buf);
+	});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
