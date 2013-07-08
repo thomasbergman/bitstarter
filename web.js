be330b58fdfc15639c6e1954f8553f79b1bd8868
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-07-09 00:51:30 +0200

    index

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
   }); 
 
-var indexToDisplay = fs.readFileSync(index.html);
-console.log(indexToDisplay);
+//var indexToDisplay = fs.readFileSync('index.html');
+//console.log(indexToDisplay);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
