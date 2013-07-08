75bf169463f9b7f21eb4a7288839aa7f3f834dd7
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-07-09 00:26:04 +0200

    Modified web.js

--- web.js
+++ web.js
@@ -4,7 +4,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
-});
+  }); 
+
+var indexToDisplay = fs.readFileSync(index.html);
+console.log(indexToDisplay);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
