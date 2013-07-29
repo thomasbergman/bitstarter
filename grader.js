94f48a92f11f49b404737ce9537e6cd0b2051f0c
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-07-29 23:42:08 +0200

    Working on HW4

--- grader.js
+++ grader.js
@@ -24,51 +24,80 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var rest = require('restler');
+
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+  var instr = infile.toString();
+  if(!fs.existsSync(instr)) {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  }
+  return instr;
+};
+
+var cheerioFromString = function(string) {
+  return cheerio.load(string);
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+  return cheerioFromString(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+  return JSON.parse(fs.readFileSync(checksfile));
 };
 
+var getRemote = function(url, afterReceive) {
+  var responseBody = function(result, response) {
+    if (result instanceof Error) {
+      console.error('Error: ' + util.format(response.message));
+    } else {
+      afterReceive(result.toString());
+    }
+  };
+  rest.get(url).on('complete', responseBody);
+}
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+  $ = cheerioHtmlFile(htmlfile);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
+};
+
+var checkRemote = function(url, checksfile) {
+  getRemote(url, function (body) {
+    $ = cheerioFromString(body);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+      var present = $(checks[ii]).length > 0;
+      out[checks[ii]] = present;
     }
-    return out;
+    var outJson = JSON.stringify(out, null, 2);
+    console.log(outJson);
+  });
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+  // Workaround for commander.js issue.
+  // http://stackoverflow.com/a/6772648
+  return fn.bind({});
 };
 
 if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+  program
+    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL to index.html')
+    .parse(process.argv);
+
+  checkRemote(program.url, program.checks);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+  exports.checkRemote = checkRemote;
 }
